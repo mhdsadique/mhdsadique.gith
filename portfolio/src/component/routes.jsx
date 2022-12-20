9187e9d33af14b0755c0e7cdfa88be30cd7b8b93
Muhammed sadique <mhdsadique8@gmail.com>
2022-12-20 09:09:13 +0530

    portfolio

--- portfolio/src/component/routes.jsx
+++ portfolio/src/component/routes.jsx
@@ -1,24 +1,24 @@
 
 
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 
-// import React from 'react'
-import { Home } from "../pages/home";
-import { About } from "../pages/about";
-import { Contact } from "../pages/contact";
-import { Project } from "../pages/project";
-import { Skills } from "../pages/skills";
+// // import React from 'react'
+// import { Home } from "../pages/home";
+// import { About } from "../pages/about";
+// import { Contact } from "../pages/contact";
+// import { Project } from "../pages/project";
+// import { Skills } from "../pages/skills";
 
-export const Routerpage = () => {
-  return (
-    <div>
-             <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="project" element={<Project />} />
-        <Route path="skill" element={<Skills />} />
-      </Routes>
-    </div>
-  )
-}
+// export const Routerpage = () => {
+//   return (
+//     <div>
+//              <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="about" element={<About />} />
+//         <Route path="contact" element={<Contact />} />
+//         <Route path="project" element={<Project />} />
+//         <Route path="skill" element={<Skills />} />
+//       </Routes>
+//     </div>
+//   )
+// }
